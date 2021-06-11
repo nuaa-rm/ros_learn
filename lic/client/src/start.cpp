c91bcefc29e3fc3de52019a5488cbb578a64748e
LIC <meecli@nuaa.edu.cn>
2021-06-11 16:28:54 +0800

    why?

--- lic/client/src/start.cpp
+++ lic/client/src/start.cpp
@@ -1,13 +1,16 @@
 #include <ros/ros.h>
 #include "client/name.h"
 #include "client/topic_name.h"
+void nameInfoCallback(const client::name::ConstPtr& msg){
+	
+	ROS_INFO("Subcriber Info: name:%s", msg->name.c_str());
+}
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "start");
 
 	ros::NodeHandle n;
-
-	//ros::Publisher name_info_pub = n.advertise<client::name>("/name_info", 10);
+	ros::Subscriber name_info_sub = n.subscribe("/name_info", 10, nameInfoCallback);
 	
 	ros::service::waitForService("/show_topic");
 	ros::ServiceClient client_name = n.serviceClient<client::topic_name>("/show_topic");
@@ -20,23 +23,7 @@ int main(int argc, char **argv){
 	
 	ROS_INFO("Result : %s", srv.response.result.c_str());
 
-	/*ros::Rate loop_rate(1);
-
-	int count = 0;
-
-	while(ros::ok()){
-		
-			client::name name_msg;
-			name_msg.name="/name_info";
-
-			name_info_pub.publish(name_msg);
-
-			ROS_INFO("Publish Name Info: namperson_msge:%s ",
-					name_msg.name.c_str());
-
-			loop_rate.sleep();
-	}*/
-
+	ros::spin();
 	return 0;
 
 }
--- lic/server/src/start_server.cpp
+++ lic/server/src/start_server.cpp
@@ -1,13 +1,9 @@
 #include <ros/ros.h>
 #include "client/name.h"
 #include "client/topic_name.h"
-/*void nameInfoCallback(const client::name::ConstPtr& msg){
-	
-	ROS_INFO("Subcriber Info: name:%s", 
-			msg->name.c_str());
-}*/
-bool topicCallback(client::topic_name::Request  &req,
-         			client::topic_name::Response &res)
+/**/
+int flag=0;
+bool topicCallback(client::topic_name::Request  &req,client::topic_name::Response &res)
 {
     ROS_INFO("Topic: name:%s  ", req.name.c_str());
     res.result = "OK";
@@ -18,11 +14,20 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "start_server");
 
 	ros::NodeHandle n;
-
-	/*ros::Subscriber name_info_sub = n.subscribe("/name_info", 10, nameInfoCallback);
-
-	*/
 	ros::ServiceServer client_name = n.advertiseService("/show_topic", topicCallback);
+	
+	ros::Publisher name_info_pub = n.advertise<client::name>("/name_info", 10);
+
+	ros::Rate loop_rate(1);
+	int count = 0;
+	while(ros::ok()){
+		
+			client::name name_msg;
+			name_msg.name="/name_info";
+			name_info_pub.publish(name_msg);
+			ROS_INFO("Publish Name Info: namperson_msge:%s ", name_msg.name.c_str());
+			loop_rate.sleep();
+	}
 	ros::spin();
 	return 0;
 }
