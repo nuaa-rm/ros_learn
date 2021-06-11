2aef164b902c4a17d05e47f3a24b51674e73655e
LIC <meecli@nuaa.edu.cn>
2021-06-11 15:47:17 +0800

    srv_ok

--- lic/client/src/start.cpp
+++ lic/client/src/start.cpp
@@ -1,15 +1,26 @@
 #include <ros/ros.h>
 #include "client/name.h"
-
+#include "client/topic_name.h"
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "start");
 
 	ros::NodeHandle n;
 
-	ros::Publisher name_info_pub = n.advertise<client::name>("/name_info", 10);
+	//ros::Publisher name_info_pub = n.advertise<client::name>("/name_info", 10);
+	
+	ros::service::waitForService("/show_topic");
+	ros::ServiceClient client_name = n.serviceClient<client::topic_name>("/show_topic");
+	client::topic_name srv;
+	srv.request.name="/name_info";
+	
+	ROS_INFO("Call service name : %s",srv.request.name.c_str());
 
-	ros::Rate loop_rate(1);
+	client_name.call(srv);
+	
+	ROS_INFO("Result : %s", srv.response.result.c_str());
+
+	/*ros::Rate loop_rate(1);
 
 	int count = 0;
 
@@ -24,7 +35,7 @@ int main(int argc, char **argv){
 					name_msg.name.c_str());
 
 			loop_rate.sleep();
-	}
+	}*/
 
 	return 0;
 
--- lic/server/src/start_server.cpp
+++ lic/server/src/start_server.cpp
@@ -1,21 +1,28 @@
 #include <ros/ros.h>
 #include "client/name.h"
-
-void nameInfoCallback(const client::name::ConstPtr& msg){
+#include "client/topic_name.h"
+/*void nameInfoCallback(const client::name::ConstPtr& msg){
 	
 	ROS_INFO("Subcriber Info: name:%s", 
 			msg->name.c_str());
+}*/
+bool topicCallback(client::topic_name::Request  &req,
+         			client::topic_name::Response &res)
+{
+    ROS_INFO("Topic: name:%s  ", req.name.c_str());
+    res.result = "OK";
+    return true;
 }
-
 int main(int argc, char **argv){
 	
 	ros::init(argc, argv, "start_server");
 
 	ros::NodeHandle n;
 
-	ros::Subscriber name_info_sub = n.subscribe("/name_info", 10, nameInfoCallback);
+	/*ros::Subscriber name_info_sub = n.subscribe("/name_info", 10, nameInfoCallback);
 
+	*/
+	ros::ServiceServer client_name = n.advertiseService("/show_topic", topicCallback);
 	ros::spin();
-
 	return 0;
 }
