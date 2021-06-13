936ae9379a128f0efb85a07a200f9d54468bf49a
dovejh <1358446393@qq.com>
2021-06-13 23:41:33 +0800

    learn to build service

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dovejh/ros_learn/ros_ws/src
+# Build directory: /home/dovejh/ros_learn/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("LiuJianhang/ser")
---
+++ build/LiuJianhang/ser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dovejh/ros_learn/ros_ws/src/LiuJianhang/ser
+# Build directory: /home/dovejh/ros_learn/ros_ws/build/LiuJianhang/ser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
