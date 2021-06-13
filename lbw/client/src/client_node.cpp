41f84aea09edab7f124a3d6a0e7520ed00ae1d99
lbw <1908127172>
2021-06-14 00:19:36 +0800

    finished without ros::Time,I will add it in later

--- lbw/client/src/client_node.cpp
+++ lbw/client/src/client_node.cpp
@@ -1,36 +1,36 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
 #include <iostream>
 #include <time.h>
-#include <client/Time.h>
+#include <geometry_msgs/Twist.h>
+#include "client/Time.h"
+
 using namespace std;
 int main(int argc, char **argv)
 {
-        time_t tt;
-        time( &tt );
-        tt = tt + 8*3600;  // transform the time zone
-        tm* t= gmtime( &tt );
-        cout <<t->tm_hour<<t->tm_min<< t->tm_sec;
-
-        // ROS节点初始化
-	ros::init(argc, argv, "client");
-        // 创建节点句柄
-	ros::NodeHandle n;
-	// 创建一个Publisher，发布名为client_topic的topic，消息类型为time，队列长度10
-        ros::Publisher client_topic_pub = n.advertise<client::Time>("client_topic", 10);
-	// 设置循环的频率
-	ros::Rate loop_rate(1);
-        while(ros::ok())
-        {
-        //初始化time
-        client::Time time;
-        time.hour =t->tm_hour;
-        time.min  =t->tm_min; 
-        time.name ="client";
-        //发布消息
-        client_topic_pub.publish(time);
-    	// 按照循环频率延时
-	loop_rate.sleep();    
-        }
-        return 0;
+    // ROS节点初始化
+    ros::init(argc, argv, "client");
+    // 创建节点句柄
+    ros::NodeHandle n;
+    // 创建一个Publisher，发布名为client_topic的topic，消息类型为time，队列长度10
+    ros::Publisher client_topic_pub = n.advertise<client::Time>("client_topic", 10);
+    // 设置循环的频率
+    ros::Rate loop_rate(1);
+    while(ros::ok())
+    {
+    //初始化time
+    time_t tt;
+    time( &tt );
+    tt = tt + 8*3600;  // transform the time zone
+    tm* t= gmtime( &tt );
+    client::Time time;
+    time.hour = t->tm_hour;
+    time.min = t->tm_min; 
+    time.sec = t->tm_sec;
+    time.name = "client";
+    //发布消息
+    client_topic_pub.publish(time);
+    // 按照循环频率延时
+    loop_rate.sleep();    
+    }
+    return 0;
 }
--- lbw/server/src/server_node.cpp
+++ lbw/server/src/server_node.cpp
@@ -1,14 +1,17 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include "learning_topic/Person.h"
+#include "client/Time.h"
+#include <iostream>
 
+using namespace std;
 void personInfoCallback(const client::Time::ConstPtr& time)
 {
     // 将接收到的消息打印出来
-    ROS_INFO("Topic_name:%s   time:%d:%d", 
-    time.name, time.hour, time.min);
+    ROS_INFO("Topic_name:%s   time:%d:%d:%d", 
+    time->name.c_str(), time->hour, time->min,time->sec);
 }
-int mian(int argc, char **argv)
+
+int main(int argc, char **argv)
 {
     // 初始化ROS节点
     ros::init(argc, argv, "server");
@@ -18,8 +21,7 @@ int mian(int argc, char **argv)
     // 创建一个Subscriber，订阅名为client_topic的topic，注册回调函数personInfoCallback
     ros::Subscriber client_topic_sub = n.subscribe("/client_topic", 10, personInfoCallback);
 
-
     ros::spin();
-
+   
     return 0;
 }
