d2295ae928e7c381fe4f9d8463130fdd0a131bfd
LIC <meecli@nuaa.edu.cn>
2021-06-15 11:03:57 +0800

    ok

--- lic/server/src/start_server.cpp
+++ lic/server/src/start_server.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-int flag = 0;
+int num_of_sub = 0;
 string aim_topic;
+ros::Subscriber name_info_sub[100];
 
 void nameInfoCallback(const client::name::ConstPtr& msg) {
     ROS_INFO("Subcriber Info: name:%s Time:%lld", msg->name.c_str() ,msg->tim.sec);
@@ -15,9 +16,10 @@ void nameInfoCallback(const client::name::ConstPtr& msg) {
 bool topicCallback(client::topic_name::Request  &req,client::topic_name::Response &res)
 {
     ROS_INFO("Topic: name:%s  ", req.name.c_str());
-	aim_topic = "/"+req.name;
+    aim_topic = "/"+req.name;
     res.result = aim_topic;
-	flag = 1;
+    ros::NodeHandle n;
+    name_info_sub[num_of_sub++] = n.subscribe<client::name>(aim_topic.c_str(), 10, nameInfoCallback);
     return true;
 }
 
@@ -26,17 +28,8 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;
 	ros::ServiceServer client_name = n.advertiseService("/show_topic", topicCallback);
 	//ros::Subscriber name_info_sub = n.subscribe<client::name>("/client0", 10, nameInfoCallback);
-	ros::Subscriber name_info_sub;
-	ros::Rate loop_rate(1);
-	while(ros::ok()) {
-		ros::spinOnce();
-		if(flag == 1) {
-			ROS_INFO("aim_topic :%s", aim_topic.c_str());
-			name_info_sub = n.subscribe<client::name>(aim_topic.c_str(), 10, nameInfoCallback);
-			flag = 0;
-		}
-		loop_rate.sleep();
-	}
+	//ros::Subscriber name_info_sub;
+	ros::spin();
 	return 0;
 }
 
