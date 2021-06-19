8f1c0a5920851be060d7b981e4da8c2937f5bc30
lbw <1908127172>
2021-06-20 02:12:22 +0800

    aba aba

--- lbw/client/src/client_node.cpp
+++ lbw/client/src/client_node.cpp
@@ -1,10 +1,9 @@
-#include "client/Time.h"
-#include "client/show.h"
+// Copyright 2021 Libowen
+#include <client/Time.h>
+#include <client/show.h>
 #include <ros/time.h>
 #include <ros/ros.h>
-using namespace std;
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     // ROS节点初始化
     ros::init(argc, argv, "client_topic");
     // 创建节点句柄
@@ -14,22 +13,22 @@ int main(int argc, char **argv)
     ros::ServiceClient cli = n.serviceClient<client::show>("show_name");
 
     client::show srv;
-    srv.request.req=1;
+    srv.request.req = 1;
     cli.call(srv);
-    ros::Publisher client_topic_pub = n.advertise<client::Time>(srv.response.rep.c_str(), 10);
+    ros::Publisher client_topic_pub = 
+    n.advertise<client::Time>(srv.response.rep.c_str(), 10);
     // 设置循环的频率
     ros::Rate loop_rate(1);
-    while(ros::ok())
-    {
-    //初始化time
+    while(ros::ok()){
+    // 初始化time
     ros::Time begin = ros::Time::now();
     client::Time time;
-    time.sec = (long unsigned int)begin.toSec();  
+    time.sec = (long unsigned int)begin.toSec();
     time.name = "client";
-    //发布消息
+    // 发布消息
     client_topic_pub.publish(time);
     // 按照循环频率延时
-    loop_rate.sleep();    
+    loop_rate.sleep();
     }
     return 0;
 }
--- lbw/server/src/server_node.cpp
+++ lbw/server/src/server_node.cpp
@@ -1,37 +1,28 @@
+// Copyright 2021 Libowen
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include "client/Time.h"
-#include "client/show.h"
-#include <iostream>
-#include <string.h>
-using namespace std;
-void timeInfoCallback(const client::Time::ConstPtr& time)
-{
+#include <client/Time.h>
+#include <client/show.h>
+void timeInfoCallback(const client::Time::ConstPtr& time){
     // 将接收到的消息打印出来
-    ROS_INFO("Topic_name:%s   time:%lu", 
+    ROS_INFO("Topic_name:%s   time:%lu",
              time->name.c_str(),time->sec);
 }
-
-bool nameCallback(client::show::Request  &req,
-                  client::show::Response &res)
-{
+bool nameCallback(client::show::Request &req ,
+                  client::show::Response &res){
     res.rep = "client_topic";
     ROS_INFO("%s",res.rep.c_str());
     return true;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     // 初始化ROS节点
     ros::init(argc, argv, "server");
     // 创建节点句柄
     ros::NodeHandle n;
     // 创建一个Subscriber，订阅名为client_topic的topic，注册回调函数timeInfoCallback
-    ros::Subscriber client_topic_sub = n.subscribe("/client_topic", 10, timeInfoCallback);
-
+    ros::Subscriber client_topic_sub = n.subscribe("/client_topic", 10,
+                                                   timeInfoCallback);
     ros::ServiceServer service = n.advertiseService("show_name", nameCallback);
-
     ros::spin();
-   
     return 0;
 }
