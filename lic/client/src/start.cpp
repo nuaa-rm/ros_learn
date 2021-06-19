75a010a9e9d338974cf91467aa61cef1a5c5724b
LIC <meecli@nuaa.edu.cn>
2021-06-19 18:13:55 +0800

    debuging

--- lic/client/src/start.cpp
+++ lic/client/src/start.cpp
@@ -1,3 +1,4 @@
+//Copyright 2021 Copyright Lic
 #include <ros/ros.h>
 #include "client/name.h"
 #include "client/topic_name.h"
@@ -10,16 +11,18 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "start");
     ros::NodeHandle n;
     ros::service::waitForService("/show_topic");
-    ros::ServiceClient client_name = n.serviceClient<client::topic_name>("/show_topic");
+    ros::ServiceClient client_name = 
+    n.serviceClient<client::topic_name>("/show_topic");
     client::topic_name srv;
     srv.request.name = "client0";
     ROS_INFO("Call service : %s", srv.request.name.c_str());
     client_name.call(srv);
     ROS_INFO("Result : %s", srv.response.result.c_str());
     ROS_INFO("Info:%s ", srv.response.result.c_str());
-    ros::Publisher name_info_pub = n.advertise<client::name>(srv.response.result.c_str(), 10);
+    ros::Publisher name_info_pub = 
+    n.advertise<client::name>(srv.response.result.c_str(), 10);
     ros::Rate loop_rate(1);
-    while(ros::ok()) {
+    while (ros::ok()) {
     	client::name name_msg;
 	name_msg.name = "client0";
 	name_msg.tim = ros::Time::now();
