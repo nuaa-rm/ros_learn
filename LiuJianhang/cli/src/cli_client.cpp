2c244fe50d43ae5fb31c9d073c7ca40b10478034
dovejh <1358446393@qq.com>
2021-06-20 17:15:57 +0800

    我觉着完成了，我不想改名了

--- LiuJianhang/cli/src/cli_client.cpp
+++ LiuJianhang/cli/src/cli_client.cpp
@@ -1,6 +1,7 @@
 #include<ros/ros.h>
 #include<ser/name.h>
 #include<ser/tim.h>
+/*
 long comYearSecond = 31536000;
 long loopYearSecond = 31622400;
 bool loopYear(int y)
@@ -13,7 +14,7 @@ bool loopYear(int y)
 	else if(y % 4 == 0)return 1;
 	else return 0;
 }
-int TimeYear(double& t)
+int TimeYear(long long& t)
 {
 	int count = 1970;
 	for(;t > (loopYear(count)?loopYearSecond:comYearSecond);count++ )
@@ -22,7 +23,7 @@ int TimeYear(double& t)
 	}
 	return count;
 }
-int TimeMonth(double& t, int year)
+int TimeMonth(long long& t, int year)
 {
 	int day;
 	day = t / 86400;
@@ -89,49 +90,64 @@ int TimeMonth(double& t, int year)
 	}
 		
 }
-int TimeDay(double& t)
+int TimeDay(long long& t)
 {
 	int d = t / 86400;
 	t -= (d * 86400);
-	return d;
+	return d + 1;
 }
-int TimeHour(double& t)
+int TimeHour(long long& t, int year)
 {
+	int loop = 0, com = 0;
+	for(int i = 1970; i < year; i ++)
+	{
+		if(loopYear(i))loop++;
+		else com ++;
+	}
+	t -= (com * comYearSecond + loop * loopYearSecond);
+	t = t -((long long)(t/86400)) * 86400;
 	int h = t / 3600;
 	t -= (h * 3600);
-	return h;
+	return h + 1;
 }
-int TimeMinute(double& t)
+int TimeMinute(long long& t)
 {
 	int m = t / 60;
 	t -= (m * 60);
-	return m;
-}
+	return m + 1;
+}*/
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "cli_client");
 	ros::NodeHandle n;
+	ros::Time t1 = ros::Time::now();
+	int t11 = t1.toSec();
 	ros::service::waitForService("my_time_server");
-	ros::Publisher time_info_pub = n.advertise<cli::tim>("time_info",10);
+	ros::Publisher time_info_pub = n.advertise<ser::tim>("time_info",10);
 	ros::Rate loop_rate(1);
 	ros::ServiceClient cli_client = n.serviceClient<ser::name>("my_time_server");
 	ser::name srv;
-	srv.request.topic_name = "topic_name";
+	srv.request.topic_name = "time_info";
 	ROS_INFO("Call service");
 	cli_client.call(srv);
 	ROS_INFO("Result:%s", srv.response.result.c_str());
 	while(ros::ok())
 	{
-		cli::tim time_msg;
-		ros::Time t1 = ros::Time::now();
+		ser::tim time_msg;
+		/*ros::Time t1 = ros::Time::now();
 		double t_cur = t1.toSec();
-		time_msg.year = TimeYear(t_cur);
-		time_msg.month = TimeMonth(t_cur, time_msg.year);
-		time_msg.day = TimeDay(t_cur);
-		time_msg.hour = TimeHour(t_cur);
-		time_msg.minute = TimeMinute(t_cur);
-		time_msg.second = t_cur;
-		time_msg.myName = "topic_name";
+		long long t2 = t_cur;
+		long long t3 = t_cur;
+		time_msg.year = TimeYear(t2);
+		time_msg.month = TimeMonth(t2, time_msg.year);
+		time_msg.day = TimeDay(t2);
+		time_msg.hour = TimeHour(t3, time_msg.year);
+		time_msg.minute = TimeMinute(t3);
+		time_msg.second = t3;*/
+		ros::Time t2 = ros::Time::now();
+	        int t22 = t2.toSec();
+		time_msg.tim = t22 - t11;
+		time_msg.myName = "time_info";
 	       	time_info_pub.publish(time_msg);
 		loop_rate.sleep();
 
--- LiuJianhang/ser/src/ser_server.cpp
+++ LiuJianhang/ser/src/ser_server.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "ser/name.h"
 #include "ser/tim.h"
+
 bool serCallback(ser::name::Request &req,
 		ser::name::Response &res)
 {
@@ -10,16 +11,15 @@ bool serCallback(ser::name::Request &req,
 }
 void timeInfoCallback(const ser::tim::ConstPtr& msg)
 {
-	ROS_INFO("%s %d/%d/%d %d:%d:%d", msg->myName.c_str(), 
-			msg->year, msg->month, msg->day, msg->hour, msg->minute
-			msg->second);
+	ROS_INFO("%s %d", msg->myName.c_str(), msg->tim);
 }
 int main(int argc, char **argv)
 {
+	char *i;
 	ros::init(argc, argv, "my_time_server");
 	ros::NodeHandle n;
 	ros::ServiceServer name_service = n.advertiseService("my_time_server", serCallback);
-	ros::Subscriber time_info_suc = n.subsciber("time_info", 10, timeInfoCallback);
+	ros::Subscriber time_info_suc = n.subscribe("time_info", 10, timeInfoCallback);
 	ROS_INFO("Ready!");
 	ros::spin();
 	return 0;
