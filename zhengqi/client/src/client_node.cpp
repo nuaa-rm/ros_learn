bc44f362ae6c2c8b20e16758882c2b9d6c5fa676
zq <3126264412@qq.com>
2022-02-26 13:28:21 +0800

    the first improve specification

--- zhengqi/client/src/client_node.cpp
+++ zhengqi/client/src/client_node.cpp
@@ -1,25 +1,27 @@
-#include<ros/ros.h>
-#include "client/Crist.h"
-#include<iostream>
-#include"client/topic_tim.h"
+// Copyright 2021 zhengqi
 
-int main(int argc,char **argv)
+#include <ros/ros.h>
+#include "client/Crist.h"
+#include "client/topic_tim.h"
+
+int main(int argc, char **argv)
 {
-    //初始化ros节点
-    ros::init(argc,argv,"client_node");
-    //创建节点句柄
+    // 初始化ros节点 
+    ros::init(argc, argv, "client_node");
+    // 创建节点句柄
     ros::NodeHandle node;
-    //等待服务建立
+    // 等待服务建立
     ros::service::waitForService("/show_name");
-    //创建一个服务客户端，连接名为/show_name的服务
-    ros::ServiceClient name_client=node.serviceClient<client::Crist>("/show_name");
-    //初始化client::Crist的数据内容
+    // 创建一个服务客户端，连接名为/show_name的服务
+    ros::ServiceClient name_client = node.serviceClient<client::Crist>("/show_name");
+    // 初始化client::Crist的数据内容
     client::Crist srv;
-    srv.request.name="Crist";
-    //调用服务
-    if(name_client.call(srv))
+    srv.request.name = "Crist";
+
+    // 调用服务
+    if (name_client.call(srv))
     {
-        ROS_INFO("result: %s", srv.response.result.c_str());
+        ROS_INFO("result: %s",  srv.response.result.c_str());
         ROS_INFO("Succeed in calling service show_name");
     }
     else
@@ -27,22 +29,24 @@ int main(int argc,char **argv)
         ROS_ERROR("Failed to call service show_name");
         return 1;
     }
-    //创建publisher，数据类型为client::topic_tim,发布名为"info_msg",队列长度10
-    ros::Publisher inform_msg_pub=node.advertise<client::topic_tim>("info_msg",10);
-    //设置循环频率
+
+    // 创建publisher，数据类型为client::topic_tim,发布名为"info_msg",队列长度10
+    ros::Publisher inform_pub = node.advertise<client::topic_tim>("info_msg", 10);
+    // 设置循环频率
     ros::Rate loop_rate(1);
+
     while(ros::ok())
     {
-        //初始化client::topic_tim的数据
-        client::topic_tim information;
-        information.name="crist";
-        information.time_now=ros::Time::now();
-        //发布消息
-        inform_msg_pub.publish(information);
-
-        ROS_INFO("publish information:  name:%s  ,time:%lld",information.name.c_str(),information.time_now.sec);
-        //按照循环频率延时
+        // 初始化client::topic_tim的数据
+        client::topic_tim Info;
+        Info.name = "crist";
+        Info.time_now = ros::Time::now();
+        // 发布消息
+        inform_pub.publish(Info);
+        ROS_INFO("publish Info:  name:%s ,time:%lld", Infor.name.c_str(),Infor.time_now.sec);
+        // 按照循环频率延时
         loop_rate.sleep();
     }
     return 0;
+
 }
--- zhengqi/server/src/server_node.cpp
+++ zhengqi/server/src/server_node.cpp
@@ -1,33 +1,36 @@
-#include<ros/ros.h>
-#include"client/Crist.h"
-#include"client/topic_tim.h"
-bool show_data(const client::topic_tim::ConstPtr& msg)
-{
-    //打印接收到的数据
-    ROS_INFO("Subsrcibe data name:%s,time:%d",msg->name.c_str(),msg->time_now.sec);
+// Copyright 2021 zhengqi
+
+#include <ros/ros.h>
+#include "client/Crist.h"
+#include "client/topic_tim.h"
+
+bool show_data(const client::topic_tim::ConstPtr& msg){
+    // 打印接收到的数据
+    ROS_INFO("Subsrcibe data name:%s,time:%d", 
+                            msg->name.c_str(), msg->time_now.sec);
 }
 
-bool Callback(client::Crist::Request &req,
-                                         client::Crist::Response &res)
-{
-   ros::NodeHandle node;
-   //显示请求数据
-    ROS_INFO("name:%s",req.name.c_str());
-    //设置返回数据
-    res.result="OK";
-    //订阅topic,注册回调函数show_data
-    ros::Subscriber inform_msg_sub=node.subscribe<client::topic_tim>("/info_msg",10,show_data);
+bool Callback(client::Crist::Request & req,
+                              client::Crist::Response & res){   
+   ros::NodeHandle n;
+   // 显示请求数据
+    ROS_INFO("name:%s", req.name.c_str());
+    // 设置返回数据
+    res.result = "OK";
+    // 订阅topic,注册回调函数show_data
+    ros::Subscriber inform_sub = n.subscribe<client::topic_tim>("/info_msg",10,show_data);
 }
-int main(int argc,char **argv)
+
+int main(int argc, char **argv)
 {
-    //初始化节点
-    ros::init(argc,argv,"server_node");
-    //设置节点句柄
-    ros::NodeHandle node;
-    //创建名为"/show_name",的server,注册回调函数Callback
-    ros::ServiceServer  name_server=node.advertiseService("/show_name",Callback);
-    //等待循环回调函数
+    // 初始化节点
+    ros::init(argc, argv, "server_node");
+    // 设置节点句柄
+    ros::NodeHandle n;
+    // 创建名为"/show_name",的server,注册回调函数Callback
+    ros::ServiceServer  name_server = n.advertiseService("/show_name",Callback);
+    // 等待循环回调函数
     ros::spin();
-
     return 0;
+
 }
