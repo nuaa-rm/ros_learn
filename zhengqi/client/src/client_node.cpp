1b02a2289ea24b668f1bc0df519a81b362ec2e73
zq <3126264412@qq.com>
2022-03-01 21:15:46 +0800

    修改代码规范

--- zhengqi/client/src/client_node.cpp
+++ zhengqi/client/src/client_node.cpp
@@ -13,11 +13,11 @@ int main(int argc, char **argv) {
     // 等待服务建立
     ros::service::waitForService("/show_name");
     // 创建一个服务客户端，连接名为/show_name的服务
-    ros::ServiceClient name_client = n.serviceClient<client::Crist>("/show_name");
+    ros::ServiceClient name_client =
+	    n.serviceClient<client::Crist>("/show_name");
     // 初始化client::Crist的数据内容
     client::Crist srv;
     srv.request.name = "Crist";
-
     // 调用服务
     if (name_client.call(srv)) {
         ROS_INFO("result: %s",  srv.response.result.c_str());
@@ -26,12 +26,10 @@ int main(int argc, char **argv) {
         ROS_ERROR("Failed to call service show_name");
         return 1;
     }
-
     // 创建publisher，数据类型为std_msgs::Header,发布名为"head",队列长度10
     ros::Publisher inform_pub = n.advertise<std_msgs::Header>("head", 10);
     // 设置循环频率
     ros::Rate loop_rate(1);
-
     while (ros::ok()) {
         // 初始化std_msgs::Header的数据
         std_msgs::Header Info;
--- zhengqi/server/src/server_node.cpp
+++ zhengqi/server/src/server_node.cpp
@@ -10,15 +10,16 @@ bool show_data(const std_msgs::Header::ConstPtr& msg) {
              msg->frame_id.c_str(), msg->stamp.sec);
 }
 
-bool Callback(client::Crist::Request & req,
-              client::Crist::Response & res) {   
+bool Callback(client::Crist::Request& req,
+              client::Crist::Response& res) {   
    ros::NodeHandle n;
    // 显示请求数据
     ROS_INFO("name:%s", req.name.c_str());
     // 设置返回数据
     res.result = "OK";
     // 订阅topic,注册回调函数show_data
-    ros::Subscriber inform_sub = n.subscribe<std_msgs::Header>("head",10,show_data);
+    ros::Subscriber inform_sub = 
+	    n.subscribe<std_msgs::Header>("head",10,show_data);
 }
 
 int main(int argc, char **argv) { 
@@ -27,7 +28,8 @@ int main(int argc, char **argv) {
     // 设置节点句柄
     ros::NodeHandle n;
     // 创建名为"/show_name",的server,注册回调函数Callback
-    ros::ServiceServer  name_server = n.advertiseService("/show_name", Callback);
+    ros::ServiceServer  name_server = 
+	    n.advertiseService("/show_name", Callback);
     // 等待循环回调函数
     ros::spin();
     return 0;
