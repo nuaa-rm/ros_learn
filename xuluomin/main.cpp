c90a299b0e001130aaaa283c6ad498ae169d9fc4
robin <Robin020813@163.com>
2022-06-20 19:26:40 +0800

    []delete the file of main

--- xuluomin/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by robin on 22-6-14.
-//
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hello World.!"<<endl;
-}
--- xuluomin/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by robin on 22-6-14.
-//
-
-#ifndef ROS_LEARN_MAIN_H
-#define ROS_LEARN_MAIN_H
-
-#endif //ROS_LEARN_MAIN_H
