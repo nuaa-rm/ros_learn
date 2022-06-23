f6e4edecdc5e9d7216971b4372ea62571394512d
andykong <andy.kong@nuaa.edu.cn>
2022-06-23 22:40:05 +0800

    [feat]part funcs

--- konghao/client/src/client.cpp
+++ konghao/client/src/client.cpp
@@ -3,30 +3,39 @@
 //
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+#include <client/num.h>
+#include <client/print.h>
 #include <sstream>
 
+#include <ros/time.h>
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "talker");
+    ros::init(argc, argv, "client");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    ros::Rate loop_rate(10);
+    ros::ServiceClient sclient = n.serviceClient<client::show>("show_name");
+    client::print srv;
+    srv.request.name ="print";
+    ros::Rate loop_rate(1);
 
+    if (sclient.call(srv)) {
+        ROS_INFO("result: %s",  srv.response.result.c_str());
+        ROS_INFO("Succeed");
+    } else {
+        ROS_ERROR("Failed");
+        return 1;
+    }
+    ros::Publisher inform_pub = n.advertise<std_msgs::Header>("head", 10);
     int count = 0;
     while (ros::ok())
     {
 
         std_msgs::String msg;
         std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
-        ROS_INFO("%s", msg.data.c_str());
-        chatter_pub.publish(msg);
-        ros::spinOnce();
-        loop_rate.sleep();
-        ++count;
+        std_msgs::Header time_now;
+        time_now.frame_id = "print";
+        time_now.stamp = ros::Time::now();
+        inform_pub.publish(Info_message);
     }
 
 
--- konghao/server/src/server.cpp
+++ konghao/server/src/server.cpp
@@ -2,19 +2,30 @@
 // Created by andykong on 22-6-23.
 //
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include <client/Num.h>
+#include <client/Print.h>
 
-
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const client::print::Request &req,
+                     const client::print::Response &res)
 {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
+    ros::NodeHandle n;
+    ROS_INFO("name:%s", req.name.c_str());
+    res.result = "OK";
+    ros::Subscriber client_topic_pub = n.subscribe<std_msgs::Header>("head", 10, show_data);
+}
+
+
+void print(const std_msgs::Header::ConstPtr& msg) {
+    ROS_INFO("name:%s,time:%d", msg->frame_id.c_str(), msg->stamp.sec);
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "listener");
+    ros::init(argc, argv, "server");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+    ros::ServiceServer server = n.advertiseService("server", chatterCallback);
+    ros::Subscriber sub = n.subscribe("chatter", 1000, print);
     ros::spin();
 
     return 0;
