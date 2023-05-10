2e6d98bfb6cbc159a92a1b177bf85a6dbf8defe0
wenhao <2522928862@qq.com>
2023-05-10 23:23:24 +0800

    catkin_make1

---
+++ src/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/wenhao/ros_learn/src
+# Build directory: /home/wenhao/ros_learn/src/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("client")
+subdirs("server")
---
+++ src/cmake-build-debug/client/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wenhao/ros_learn/src/client
+# Build directory: /home/wenhao/ros_learn/src/cmake-build-debug/client
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
